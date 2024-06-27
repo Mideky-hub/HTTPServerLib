55182df6e341ca267ec35b9a4b2b66dda9d803ab
Midekyy <mathieukerbel@gmail.com>
2024-06-28 01:30:45 +0200

    Fully implement socketing with proper installation. #12 : Implementation of the socket addr in the tcp server

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 #define PORT 8080
 
-
 int main() {
     tcp_server server("localhost", PORT);
     return 0;
--- src/socket_addr.hh
+++ src/socket_addr.hh
@@ -2,8 +2,10 @@
 #define SOCKET_ADDR_HH
 
 //Local libraries
+#include "logging.hh"
 
 //Third-party libraries
+#include <winsock.h>
 
 //Standard libraries
 #include <cstdint>
@@ -16,7 +18,8 @@
 
 */
 
-struct socket_addr_in /*IPv4 ADDR*/ {
+class socket_addr_in /*IPv4 ADDR*/ {
+public: // Variables
     // sin_family is the address family that the socket is going to communicate with.
     // an address family is a set of protocols that can be used to communicate between two sockets.
     // For example, AF_INET is the address family for IPv4, and AF_INET6 is the address family for IPv6.
@@ -34,6 +37,17 @@ struct socket_addr_in /*IPv4 ADDR*/ {
     // sin_zero is a padding field that is used to make the structure the same size as a sockaddr structure.
     // This is done so that the sockaddr_in structure can be used in functions that expect a sockaddr structure.
     uint8_t     sin_zero[8];
+
+public: // Methods
+    // Constructor
+    socket_addr_in(const uint16_t PORT) : sin_family(AF_INET), sin_port(htons(PORT)), sin_addr(INADDR_ANY) {
+        memset(sin_zero, 0, sizeof(sin_zero));
+    }
+
+    // Destructor
+    ~socket_addr_in() = default;
+
 };
+
 
 #endif // SOCKET_ADDR_HH
